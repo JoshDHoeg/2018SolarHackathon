2405cbbab7967c721dededf39bde6f9695e73f36
Evan Hicks <hicks2evan@gmail.com>
2018-11-10 14:36:36 -0700

    adding loading spinner

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -3,6 +3,7 @@ import MyMap from './myMap'
 import PvWatts from './pvWatts'
 import UtilityUsage from './utilityUsage'
 import ROI from './roi';
+import CircularIndeterminate from './loader'
 
 export default class Dashboard extends React.Component {
   constructor(props) {
@@ -10,20 +11,35 @@ export default class Dashboard extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <ROI value={1200} />
+    if(this.props.address != null) {
+      if(this.props.ready) {
+        return (
+          <div>
+            <ROI value={1200} />
 
-        <p>Address: {this.props.address}</p>
-        <p>Latitude: {this.props.lat}</p>
-        <p>Longitude: {this.props.lon}</p>
-        <p>ZipCode: {this.props.zip}</p>
-        <p>State: {this.props.state}</p>
-        <p>City: {this.props.city}</p>
-        <PvWatts {...this.props.pvwatt}/>
-        <UtilityUsage />
-        <MyMap {...this.props}/>
-      </div>
-    );
+            <p>Address: {this.props.address}</p>
+            <p>Latitude: {this.props.lat}</p>
+            <p>Longitude: {this.props.lon}</p>
+            <p>ZipCode: {this.props.zip}</p>
+            <p>State: {this.props.state}</p>
+            <p>City: {this.props.city}</p>
+            <PvWatts {...this.props.pvwatt}/>
+            <UtilityUsage />
+            <MyMap {...this.props}/>
+          </div>
+        );
+      }
+      else if(this.props.loading) {
+        return (
+        <CircularIndeterminate></CircularIndeterminate>
+        );
+      }
+      else {
+        return (<div></div>);
+      }
+    }
+    else {
+      return (<div></div>);
+    }
   }
 }
