d6a737560f6e3df7e6833d3103fccd739b1d489e
Evan Hicks <hicks2evan@gmail.com>
2018-11-10 10:34:23 -0700

    adding map_component basics

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import MyMap from './myMap'
 
 export default class Dashboard extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+        <MyMap/>
+      </div>
+    );
   }
 }
