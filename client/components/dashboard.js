8bbdeb6f9008952684b5fa47afed7b9a8f08c2a5
Evan Hicks <hicks2evan@gmail.com>
2018-11-10 11:40:31 -0700

    integrating react google maps comp

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import MyMap from './myMap'
 
+
 export default class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        <MyMap lat={this.props.lat} long={this.props.long}/>
+        <MyMap lat={this.props.lat} long={this.props.lng}/>
       </div>
     );
   }
