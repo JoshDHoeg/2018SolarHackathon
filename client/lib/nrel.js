68bdc1b0e1d7b90ca292a96b34f46fe252ee6d67
Zac Holland <zacharyholland@gmail.com>
2018-11-10 10:53:38 -0700

    added energy expenditure

--- client/lib/nrel.js
+++ client/lib/nrel.js
@@ -1,29 +1,31 @@
 import axios from 'axios';
 
 const apikey = 'i9p7npVWHIhIV1vlQTMBGIUlrdHpf2bxVajXyeiH';
-const baseUrl = 'https://cors-anywhere.herokuapp.com/https://developer.nrel.gov/'
+const baseUrl = 'https://cors-anywhere.herokuapp.com/https://developer.nrel.gov'
 /**
  * 
- * @param {string} city City name
- * @param {string} state_abbr Abbreviation for State
- * @param {number} zip Zip code
+ * @param {object} params {city, state_abbr, zip}
  */
-export function GetExpendituresGHGBySector(city, state_abbr, zip) {
-  let params = {}
-
-  if (zip) {
-    params = {
-      zip
-    };
-  } else {
-    params = {
-      city,
-      state_abbr
-    }
-  }
+export function GetExpendituresGHGBySector(params) {
   return axios.create({
     baseURL: `${baseUrl}/api/cleap/v1/energy_expenditures_and_ghg_by_sector`,
     headers: {'X-Api-Key': apikey},
     params
+  }).get();
+}
+
+/**
+ * 
+ * @param {Object} params {system_capacity, module_type, losses, array_type, tilt, azimuth, address, lat, lon, file_id, dataset, radius, timeframe, ... etc}
+ */
+export function GetPVWatts(params) {
+  return axios.create({
+    method: 'get',
+    baseURL: `${baseUrl}/api/pvwatts/v6.format/`,
+    params: {
+      api_key: apikey,
+      format: 'json',
+      ...params
+    }
   }).get();
 }
