6f239fa3bf1a3517bdd8de383e6a6b064d62fb28
Josh Hoeg <josh.hoeg@du.edu>
2018-11-10 09:35:08 -0700

    edited the index.js page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,90 +2,52 @@ import React from 'react'
 import Link from 'next/link'
 import Head from '../components/head'
 import Nav from '../components/nav'
+import Dashboard from '../components/dashboard'
 
-const Home = () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+export default class Home extends React.Component {
 
-      <div className="row">
-        <Link href="/dashboard">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the{' '}
-              <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
-          </a>
-        </Link>
+  constructor() {
+    super();
+    this.state = {address: '', lat: '', long: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({address: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.loadData();
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Head title="Home" />
+        <Nav />
+
+
+        <header className="App-header" >
+          <div className="App-header-info">
+            <h1 className="App-title">Welcome to Daddy</h1>
+            <p>Enter an address to find the cost of a solar plan</p>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                <input className="input-zip" type="text" value={this.state.address} onChange={this.handleChange} />
+              </label>
+              <input className="input-submit" type="submit" value="Submit" />
+            </form>
+          </div>
+        </header>
+
+        <Dashboard />
+
       </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+    );
+  }
+}
